90ccbe95e3b79873a201f0740802378fd2abc716
kosmastsk <kosmastsk@gmail.com>
2019-03-27 19:40:36 +0200

    Add 3D navigation and path planning

---
+++ drone/drone_3d_nav/include/drone_3d_nav/navigate.h
@@ -0,0 +1,112 @@
+#include <ros/ros.h>
+#include <queue>
+
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Transform.h>
+#include <nav_msgs/Odometry.h>
+
+#include <visualization_msgs/MarkerArray.h>
+#include <visualization_msgs/Marker.h>
+
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Matrix3x3.h>
+#include <tf2/transform_datatypes.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2/impl/utils.h>
+
+#include <trajectory_msgs/MultiDOFJointTrajectory.h>
+
+namespace navigate
+{
+class Navigator
+{
+private:
+  ros::NodeHandle _nh;
+
+  ros::Publisher _vel_pub;
+
+  ros::Subscriber _waypoints_sub;
+  ros::Subscriber _odom_sub;
+
+  visualization_msgs::MarkerArray _marker_array_msg;
+
+  std::queue<geometry_msgs::Transform> _waypoints;
+  int _number_of_waypoints;
+  bool _waypoints_received;
+
+  geometry_msgs::Transform _current_goal;
+
+  float _tolerance;
+  double _dt;
+
+  // PID
+  double _kp;
+  double _ki;
+  double _kd;
+
+  double _kp_yaw;
+  double _ki_yaw;
+  double _kd_yaw;
+  // float _w;
+
+  float _error_x;
+  float _error_y;
+  float _error_z;
+  // float _error_roll;
+  // float _error_pitch;
+  float _error_yaw;
+
+  float _prev_error_x;
+  float _prev_error_y;
+  float _prev_error_z;
+  // float _prev_error_roll;
+  // float _prev_error_pitch;
+  float _prev_error_yaw;
+
+  float _rise;
+
+  float _proportional_x;
+  float _proportional_y;
+  float _proportional_z;
+  // float _proportional_roll;
+  // float _proportional_pitch;
+  float _proportional_yaw;
+
+  float _integral_x;
+  float _integral_y;
+  float _integral_z;
+  // float _integral_roll;
+  // float _integral_pitch;
+  float _integral_yaw;
+
+  float _derivative_x;
+  float _derivative_y;
+  float _derivative_z;
+  // float _derivative_roll;
+  // float _derivative_pitch;
+  float _derivative_yaw;
+
+  float _action_x;
+  float _action_y;
+  float _action_z;
+  // float _action_roll;
+  // float _action_pitch;
+  float _action_yaw;
+
+  geometry_msgs::Transform _pose;
+  geometry_msgs::Twist _twist;
+
+  bool _must_exit;
+  int _waypoint_number;
+
+  // Callbacks
+  void waypointCallback(const trajectory_msgs::MultiDOFJointTrajectoryConstPtr& msg);
+  void odomCallback(const nav_msgs::OdometryConstPtr& msg);
+
+public:
+  Navigator();
+  ~Navigator();
+};
+
+}  // namespace navigate
--- drone/drone_3d_nav/src/navigate.cpp
+++ drone/drone_3d_nav/src/navigate.cpp
@@ -1,191 +1,217 @@
-#include <ros/ros.h>
-#include <geometry_msgs/Twist.h>
-#include <geometry_msgs/PoseStamped.h>
-#include <nav_msgs/Odometry.h>
-#include <visualization_msgs/MarkerArray.h>
-#include <visualization_msgs/Marker.h>
+#include "drone_3d_nav/navigate.h"
 
-float goal[3][3] = { { 6, 0, 2 }, { 8, -2, 2.5 }, { 12, 5, 2 } };
-float tolerance = 0.2;
-
-double kp = 0.5;      // 0.5;
-double ki = 0.0002;   // 0.0002;
-double kd = 0.00005;  // 0.00005;
-
-float w;
-
-float error_x = 0;
-float error_y = 0;
-float error_z = 0;
-float error_w = 0;
-float prev_error_x = 0;
-float prev_error_y = 0;
-float prev_error_z = 0;
-float prev_error_w = 0;
-float rise = 1;
-float nonstop = false;
-
-float proportional_x = 0;
-float proportional_y = 0;
-float proportional_z = 0;
-float proportional_w = 0;
-
-float integral_x = 0;
-float integral_y = 0;
-float integral_z = 0;
-float integral_w = 0;
-
-float derivative_x = 0;
-float derivative_y = 0;
-float derivative_z = 0;
-float derivative_w = 0;
-
-float action_x = 0;
-float action_y = 0;
-float action_z = 0;
-float action_w = 0;
-
-geometry_msgs::Point real;
-geometry_msgs::Twist twist;
-
-bool must_exit = false;
-int waypoint_number = 0;
-
-// void odoCallback(const geometry_msgs::PoseStampedConstPtr& msg)
-void odoCallback(const nav_msgs::OdometryConstPtr& msg)
+namespace navigate
 {
-  real.x = msg->pose.pose.position.x;
-  real.y = msg->pose.pose.position.y;
-  real.z = msg->pose.pose.position.z;
-  w = msg->pose.pose.orientation.z;
+Navigator::Navigator()
+{
+  // Parameters
 
-  prev_error_x = error_x;
-  prev_error_y = error_y;
-  prev_error_z = error_z;
-  prev_error_w = error_w;
+  _nh.param<double>("/kp", _kp, 0.5);
+  _nh.param<double>("/ki", _ki, 0.0002);
+  _nh.param<double>("/kd", _kd, 0.00005);
+  _nh.param<double>("/kp_yaw", _kp_yaw, 0.5);
+  _nh.param<double>("/ki_yaw", _ki_yaw, 0.0002);
+  _nh.param<double>("/kd_yaw", _kd_yaw, 0.00005);
+  _nh.param<float>("/tolerance", _tolerance, 0.1);
 
-  error_x = goal[waypoint_number][0] - real.x;
-  error_y = goal[waypoint_number][1] - real.y;
-  error_z = (goal[waypoint_number][2] + 0.001 * rise) - real.z;
-  error_w = 0 - w;
+  _must_exit = false;
+  _waypoint_number = 0;
+  _waypoints_received = false;
+  _dt = 0;
 
-  proportional_x = kp * error_x;
-  proportional_y = kp * error_y;
-  proportional_z = kp * error_z;
-  proportional_w = kp * error_w;
+  _error_x = 0;
+  _error_y = 0;
+  _error_z = 0;
+  // _error_roll = 0;
+  // _error_pitch = 0;
+  _error_yaw = 0;
 
-  integral_x += ki * error_x;
-  integral_y += ki * error_y;
-  integral_z += ki * error_z;
-  integral_w += ki * error_w;
+  _prev_error_x = 0;
+  _prev_error_y = 0;
+  _prev_error_z = 0;
+  // _prev_error_roll = 0;
+  // _prev_error_pitch = 0;
+  _prev_error_yaw = 0;
 
-  derivative_x = kd * (error_x - prev_error_x);
-  derivative_y = kd * (error_y - prev_error_y);
-  derivative_z = kd * (error_z - prev_error_z);
-  derivative_w = kd * (error_w - prev_error_w);
+  _rise = 1;
 
-  action_x = proportional_x + integral_x + derivative_x;
-  action_y = proportional_y + integral_y + derivative_y;
-  action_z = proportional_z + integral_z + derivative_z;
-  action_w = 10 * proportional_w + integral_w + derivative_w;
+  _proportional_x = 0;
+  _proportional_y = 0;
+  _proportional_z = 0;
+  // _proportional_roll = 0;
+  // _proportional_pitch = 0;
+  _proportional_yaw = 0;
 
-  twist.linear.x = action_x;
-  twist.linear.y = action_y;
-  twist.linear.z = action_z;
-  twist.angular.z = action_w;
+  _integral_x = 0;
+  _integral_y = 0;
+  _integral_z = 0;
+  // _integral_roll = 0;
+  // _integral_pitch = 0;
+  _integral_yaw = 0;
 
-  ROS_INFO("Error X: %0.2f \n", error_x);
-  ROS_INFO("Error Y: %0.2f \n", error_y);
-  ROS_INFO("Error Z: %0.2f \n", error_z);
-  ROS_INFO("Error W: %0.2f \n", w);
+  _derivative_x = 0;
+  _derivative_y = 0;
+  _derivative_z = 0;
+  // _derivative_roll = 0;
+  // _derivative_pitch = 0;
+  _derivative_yaw = 0;
 
-  ROS_INFO("Action X: %0.2f \n", action_x);
-  ROS_INFO("Action Y: %0.2f \n", action_y);
-  ROS_INFO("Action Z: %0.2f \n", action_z);
-  ROS_INFO("Action W: %0.2f \n", action_w);
+  _action_x = 0;
+  _action_y = 0;
+  _action_z = 0;
+  // _action_roll = 0;
+  // _action_pitch = 0;
+  _action_yaw = 0;
 
-  ROS_INFO("Next goal %d\n", waypoint_number + 1);
+  _vel_pub = _nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
 
-  if ((fabs(error_x) < tolerance) && (fabs(error_y) < tolerance))
-  {
-    ROS_INFO("Error in accepted range. Next waypoint.\n");
-    if (must_exit == true)
-    {
-      twist.linear.x = 0;
-      twist.linear.y = 0;
-      twist.linear.z = 0;
-      twist.angular.z = 0;
-      exit(0);
-    }
-    else
-    {
-      waypoint_number += 1;
-      rise += 1;
-    }
-  }
-
-  if (waypoint_number == (sizeof(goal) / sizeof(goal[0])))
-  {
-    if (nonstop)
-    {
-      waypoint_number = 1;
-    }
-    else
-    {
-      waypoint_number = 0;
-      must_exit = true;
-    }
-  }
+  _odom_sub = _nh.subscribe("/ground_truth/state", 1, &Navigator::odomCallback, this);
+  _waypoints_sub = _nh.subscribe("/waypoints_smooth", 1, &Navigator::waypointCallback, this);
 }
+
+Navigator::~Navigator()
+{
+}
+
+void Navigator::waypointCallback(const trajectory_msgs::MultiDOFJointTrajectoryConstPtr& msg)
+{
+  // The first item in the points array is the initial position
+  _waypoint_number = 1;
+  _number_of_waypoints = msg->points.size();
+  ROS_INFO("%d waypoints received\n", _number_of_waypoints);
+  for (unsigned int i = 0; i < _number_of_waypoints; i++)
+  {
+    _waypoints.push(msg->points[i].transforms[0]);
+  }
+  _current_goal = _waypoints.front();
+  _waypoints.pop();  // Remove this element from the queue
+  _waypoints_received = true;
+}
+
+void Navigator::odomCallback(const nav_msgs::OdometryConstPtr& msg)
+{
+  // If the waypoints have not received, we cannot proceed with the navigation.
+  if (!_waypoints_received)
+  {
+    ROS_WARN_ONCE("Waypoints not received. Skipping odometry...\n");
+    return;
+  }
+
+  _pose.translation.x = msg->pose.pose.position.x;
+  _pose.translation.y = msg->pose.pose.position.y;
+  _pose.translation.z = msg->pose.pose.position.z;
+
+  _pose.rotation = msg->pose.pose.orientation;  // geometry_msgs::Quaternion showing the current orientation
+
+  // Get the Yaw from current goal and odometry measurement
+  tf2::Quaternion current_goal_quat, pose_quat;
+  tf2::fromMsg(_current_goal.rotation, current_goal_quat);
+  tf2::fromMsg(_pose.rotation, pose_quat);
+
+  double current_goal_yaw = tf2::impl::getYaw(current_goal_quat);
+  double pose_yaw = tf2::impl::getYaw(pose_quat);
+
+  _dt = ros::Time::now().toSec() - _dt;
+
+  // Find errors and PID values to calculate next action
+  _error_x = _current_goal.translation.x - _pose.translation.x;
+  _error_y = _current_goal.translation.y - _pose.translation.y;
+  _error_z = _current_goal.translation.z - _pose.translation.z;
+  // _error_roll = current_goal_roll - pose_roll;     // _current_goal.rotation.x - _pose.rotation.x;
+  // _error_pitch = current_goal_pitch - pose_pitch;  // _current_goal.rotation.y - _pose.rotation.y;
+  _error_yaw = current_goal_yaw - pose_yaw;  // _current_goal.rotation.z - _pose.rotation.z;
+
+  _proportional_x = _kp * _error_x;
+  _proportional_y = _kp * _error_y;
+  _proportional_z = _kp * _error_z;
+  // _proportional_roll = _kp * _error_roll;
+  // _proportional_pitch = _kp * _error_pitch;
+  _proportional_yaw = _kp_yaw * _error_yaw;
+
+  _integral_x = _ki * (_integral_x + _error_x * _dt);
+  _integral_y = _ki * (_integral_y + _error_y * _dt);
+  _integral_z = _ki * (_integral_z + _error_z * _dt);
+  // _integral_roll += _ki * _error_roll;
+  // _integral_pitch += _ki * _error_pitch;
+  _integral_yaw = _ki_yaw * (_integral_yaw + _error_yaw * _dt);
+
+  _derivative_x = _kd * (_error_x - _prev_error_x);
+  _derivative_y = _kd * (_error_y - _prev_error_y);
+  _derivative_z = _kd * (_error_z - _prev_error_z);
+  // _derivative_roll = _kd * (_error_roll - _prev_error_roll);
+  // _derivative_pitch = _kd * (_error_pitch - _prev_error_pitch);
+  _derivative_yaw = _kd_yaw * (_error_yaw - _prev_error_yaw);
+
+  _prev_error_x = _error_x;
+  _prev_error_y = _error_y;
+  _prev_error_z = _error_z;
+  // _prev_error_roll = _error_roll;
+  // _prev_error_pitch = _error_pitch;
+  _prev_error_yaw = _error_yaw;
+
+  _action_x = _proportional_x + _integral_x + _derivative_x;
+  _action_y = _proportional_y + _integral_y + _derivative_y;
+  _action_z = 5 * _proportional_z + _integral_z + _derivative_z;
+  // _action_roll = _proportional_roll + _integral_roll + _derivative_roll;
+  // _action_pitch = _proportional_pitch + _integral_pitch + _derivative_pitch;
+  _action_yaw = _proportional_yaw + _integral_yaw + _derivative_yaw;
+
+  _twist.linear.x = _action_x;
+  _twist.linear.y = _action_y;
+  _twist.linear.z = _action_z;
+  _twist.angular.x = 0;  //_action_roll;
+  _twist.angular.y = 0;  //_action_pitch;
+  _twist.angular.z = _action_yaw;
+
+  ROS_INFO("Error (X, Y, Z, Yaw) : (%0.2f, %0.2f, %0.2f, %0.2f) \n", _error_x, _error_y, _error_z, _error_yaw);
+
+  ROS_INFO("Action (X, Y, Z, Yaw) : (%0.2f, %0.2f, %0.2f, %0.2f) \n", _action_x, _action_y, _action_z, _action_yaw);
+
+  // Ensure that the drone's position is in accepted range error
+  if ((fabs(_error_x) <= _tolerance) && (fabs(_error_y) <= _tolerance) && (fabs(_error_z) <= _tolerance))
+  {
+    if (_must_exit == true)
+    {
+      ROS_INFO("Final waypoint reached. Exiting...\n");
+      _twist.linear.x = 0;
+      _twist.linear.y = 0;
+      _twist.linear.z = 0;
+      _twist.angular.x = 0;
+      _twist.angular.y = 0;
+      _twist.angular.z = 0;
+      _waypoint_number = 0;
+      _waypoints_received = false;  // Set it to false again, so to wait for new waypoints to serve
+    }
+    else
+    {
+      ROS_INFO("Error in accepted range. Next waypoint.\n");
+      _current_goal = _waypoints.front();
+      _waypoints.pop();  // Remove the element from the queue
+      _waypoint_number += 1;
+      _rise += 1;
+
+      ROS_INFO("Next goal %d\n", _waypoint_number + 1);
+      ROS_INFO("Coordinates (x,y,z, yaw) : (%f, %f, %f, %f)\n", _current_goal.translation.x,
+               _current_goal.translation.y, _current_goal.translation.z, current_goal_yaw);
+    }
+  }
+
+  if (_waypoint_number == _number_of_waypoints)
+  {
+    _waypoint_number = 0;
+    _must_exit = true;
+  }
+
+  _vel_pub.publish(_twist);
+}
+}  // namespace navigate
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "navigate");
 
-  ros::NodeHandle np;
-  ros::NodeHandle nh;
-  ros::Publisher pub_vel = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+  navigate::Navigator nav;
 
-  ros::Subscriber sub = np.subscribe("/ground_truth/state", 10, odoCallback);
-  // amcl_pose
-
-  // Marker for waypoints
-  ros::Publisher vis_pub = nh.advertise<visualization_msgs::MarkerArray>("visualization_marker", 100);
-
-  visualization_msgs::MarkerArray marker_array_msg;
-  marker_array_msg.markers.resize(6);
-  for (int i = 0; i < 6; i++)
-  {
-    visualization_msgs::Marker marker;
-    marker.header.frame_id = "/map";
-    marker.header.stamp = ros::Time::now();
-    marker.ns = "navigate";
-    marker.id = i;
-    marker.type = visualization_msgs::Marker::SPHERE;
-    marker.action = visualization_msgs::Marker::ADD;
-    marker.pose.position.x = goal[i][0];
-    marker.pose.position.y = goal[i][1];
-    marker.pose.position.z = goal[i][2];
-    marker.scale.x = 0.2;
-    marker.scale.y = 0.2;
-    marker.scale.z = 0.2;
-    marker.color.a = 1.0;  // Don't forget to set the alpha!
-    marker.color.r = 0.0;
-    marker.color.g = 1.0;
-    marker.color.b = 0.0;
-
-    marker_array_msg.markers[i] = marker;
-  }
-
-  ros::Rate loop_rate(10);
-  int count = 0;
-
-  while (ros::ok())
-  {
-    pub_vel.publish(twist);
-    vis_pub.publish(marker_array_msg);
-    ros::spinOnce();
-    loop_rate.sleep();
-    ++count;
-  }
+  ros::spin();
+  return 0;
 }
